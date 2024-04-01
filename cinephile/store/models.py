3720a7e09ebf45fd93a1566465190fb8306f06bb
Praveen PR <prp7840@gmail.com>
2024-04-01 13:03:37 +0530

    category model created

--- cinephile/store/models.py
+++ cinephile/store/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+    
+class Category(models.Model):
+    name=models.CharField(max_length=200,null=False,blank=False)
+    image=models.ImageField(upload_to='images',null=True)
+    
+    def __str__(self):
+        return self.name
