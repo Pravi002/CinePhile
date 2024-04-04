b607d8a00d642307db835420d5c882d5194be98f
Praveen PR <prp7840@gmail.com>
2024-04-04 12:54:56 +0530

    model "Item" updated

--- cinephile/store/models.py
+++ cinephile/store/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
     
 class Category(models.Model):
     name=models.CharField(max_length=50,null=False)
-    discription=models.TextField(max_length=600,null=True)
+    discription=models.TextField(null=True)
     
     def __str__(self):
         return self.name
@@ -18,20 +18,13 @@ class Item(models.Model):
     genre=models.CharField(max_length=50,null=False)
     release_date=models.CharField(max_length=20,null=False)
     duration=models.CharField(max_length=20,null=False)
-    summary=models.TextField(max_length=700,null=False)
+    summary=models.TextField(null=False)
     language=models.CharField(max_length=50,null=False)
     director=models.CharField(max_length=50,null=False)
-    writter=models.CharField(max_length=50,null=False)
-    leadrole=models.CharField(max_length=50,null=False)
-    actor1=models.CharField(max_length=50,null=True)
-    actor2=models.CharField(max_length=50,null=True)
-    actor3=models.CharField(max_length=50,null=True)
-    actor4=models.CharField(max_length=50,null=True)
-    actor5=models.CharField(max_length=50,null=True)
-    seasons=models.IntegerField(null=True)
-    episodes=models.IntegerField(null=True)
+    writer=models.CharField(max_length=50,null=False)
+    cast=models.CharField(max_length=100,null=False)
 
     def __str__(self):
-        return self.title
-    
+        return self.title 
+
 
